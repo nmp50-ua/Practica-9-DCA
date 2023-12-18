01dd915cb30645623d4ba7623f70f817c170212d
Noel Martínez <nmp50@alu.ua.es>
2023-12-18 16:33:24 +0100

    Primer commit

---
+++ ejemplo.py
@@ -0,0 +1,19 @@
+# Archivo: mi_programa.py
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def main():
+    num1 = 5
+    num2 = 2 
+    resultado = suma(num1, num2)
+    print(f"La suma es: {resultado}")
+
+    resultado_resta = resta(num1, num2)
+    print(f"La resta es: {resultado_resta}")
+
+if __name__ == "__main__":
+    main()
